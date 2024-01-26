65861a1c38844e84737bb08833852180e8d601ad
Marshal <olmarsh.192@gmail.com>
2024-01-26 22:38:59 +0100

    moved logic into error

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -1,29 +1,39 @@
-import { ValidationError } from "express-validator";
+import { ValidationError, FieldValidationError as ExpressFieldValidationError } from "express-validator";
 
 // Define an interface for the common properties
 interface CommonErrorProperties {
-    message: any;
-    field?: string;
+  message: any;
+  field?: string;
+}
+
+// Define the types for each specific error
+type FieldValidationError = CommonErrorProperties & ExpressFieldValidationError;
+
+type AlternativeValidationError = CommonErrorProperties & {
+  type: 'alternative';
+  nestedErrors: FieldValidationError[];
+};
+
+export class RequestValidationError extends Error {
+  statusCode = 400;
+
+  constructor(public errors: ValidationError[]) {
+    super();
+
+    // Only because we are extending a built-in class
+    Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
-  
-  // Define the types for each specific error
-  type FieldValidationError = CommonErrorProperties & {
-    type: 'field';
-    location: string; // Change the type if needed
-    path: string;
-    value: any;
-  };
-  
-  type AlternativeValidationError = CommonErrorProperties & {
-    type: 'alternative';
-    nestedErrors: FieldValidationError[];
-  };
-  
-  export class RequestValidationError extends Error {
-    constructor(public errors: ValidationError[]) {
-      super();
-  
-      // Only because we are extending a built-in class
-      Object.setPrototypeOf(this, RequestValidationError.prototype);
-    }
+
+  serializeErrors() {
+    const formattedErrors = this.errors.map((error) => {
+      if (error.type === "field") {
+        const fieldError = error as FieldValidationError;
+        return { message: fieldError.msg, field: fieldError.path };
+      }
+      // Handle other types if needed
+      return { message: 'Unknown error type' };
+    });
+
+    return formattedErrors;
   }
+}
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,10 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 import { RequestValidationError } from "../errors/request-validation-error";
 import { DatabaseConnectionError } from "../errors/database-connection-error";
-import {
-  AlternativeValidationError,
-  FieldValidationError,
-} from "express-validator";
 
 export const errorHandler = (
   err: Error,
@@ -13,21 +9,11 @@ export const errorHandler = (
   next: NextFunction
 ) => {
   if (err instanceof RequestValidationError) {
-    const formattedErrors = err.errors.map((error) => {
-      if (error.type === "field") {
-        const fieldError = error as FieldValidationError;
-        return { message: fieldError.msg, field: fieldError.path };
-      } else if (error.type === "alternative") {
-        const alternativeError = error as AlternativeValidationError;
-        // Handle nested errors if needed
-        return { message: alternativeError.msg };
-      }
-    });
-    return res.status(400).send({ errors: formattedErrors });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
   if (err instanceof DatabaseConnectionError) {
-    return res.status(500).send({ errors: [{ message: err.reason }] });
+    return res.status(err.statusCode).send({ errors: err.serializeErrors()});
   }
 
   res.status(400).send({
