d5eb3440c36305e6e34ee3f7b6f518f4da12e9d3
Marshal <olmarsh.192@gmail.com>
2024-03-06 12:18:06 +0100

    used useRequest hook

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -1,15 +1,47 @@
+import { useState } from 'react';
+import useRequest from '../../hooks/use-request';
+
 export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signup',
+    method: 'post',
+    body: {
+      email,
+      password,
+    },
+  });
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    doRequest();
+  };
+
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <h1>Sign Up</h1>
       <div className="form-group">
         <label>Email Address</label>
-        <input className="form-control" />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="form-control"
+        />
       </div>
+      {/* show validation error below the email field */}
       <div className="form-group">
         <label>Password</label>
-        <input type="password" className="form-control" />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          className="form-control"
+        />
       </div>
+      {/* show validation error below the password field */}
+      {errors}
       <button className="btn btn-primary">Sign Up</button>
     </form>
   );
